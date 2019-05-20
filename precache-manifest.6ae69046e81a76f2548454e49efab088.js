5cd386dc05946452317e790ac4959f5727271ad3
yomi05 <oriyomis@gmail.com>
2019-05-20 15:37:37 +0100

    Updates

---
+++ precache-manifest.6ae69046e81a76f2548454e49efab088.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e088e4b1e340ee67fb97",
+    "url": "/finalcourse/static/css/main.5ddb6632.chunk.css"
+  },
+  {
+    "revision": "e088e4b1e340ee67fb97",
+    "url": "/finalcourse/static/js/main.e088e4b1.chunk.js"
+  },
+  {
+    "revision": "f60110df69b77b7e6020",
+    "url": "/finalcourse/static/js/1.f60110df.chunk.js"
+  },
+  {
+    "revision": "ec1b498d23f47bf03203",
+    "url": "/finalcourse/static/js/runtime~main.ec1b498d.js"
+  },
+  {
+    "revision": "2bb3bca5f2e39226bcb6908742ac7b16",
+    "url": "/finalcourse/index.html"
+  }
+];
